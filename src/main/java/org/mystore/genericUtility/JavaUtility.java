cc5aeca0a414113e792feb24db81ca946f21c53f
Harinath <gowdharinath@gmail.com>
2022-10-25 22:32:53 +0530

    first commit

---
+++ src/main/java/org/mystore/genericUtility/JavaUtility.java
@@ -0,0 +1,11 @@
+package org.mystore.genericUtility;
+
+public class JavaUtility {
+	
+public int getRandomNumber()
+{
+	return 0;
+	
+}
+	
+}
---
+++ src/test/java/org/mystore/testNGpractice/TestCase1.java
@@ -0,0 +1,27 @@
+package org.mystore.testNGpractice;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+		
+	@Test
+	public void tesCaseMethod1()
+	{
+		System.out.println("successfully method1 executed");
+	}
+	
+	@Test
+	public void testCaseMethod2()
+	{
+		System.out.println("successfully method2 executed");
+	}
+	
+	@Test
+	public void testCaseMethod3()
+	{
+		System.out.println("successfully method3 executed");
+	}
+	
+	//first commit
+
+}
